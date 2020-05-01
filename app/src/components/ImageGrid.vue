1bf15a3a1122433e7433feb9c541befc8c6eea46
Mikhail Bushkov <realbushman@gmail.com>
2020-05-02 01:07:32 +0200

    Fixed styling.

--- app/src/components/ImageGrid.vue
+++ app/src/components/ImageGrid.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
     <div>Images:</div>
-    <div v-for="image in imageList" :key="image.uid">
-      <img :src="'http://127.0.0.1:30000/images/' + image.uid" />
+
+    <div class="container">
+      <div v-for="image in imageList" :key="image.uid" class="image-box" v-bind:style="imageBoxStyle">
+        <div class="nested">
+          <img
+            :src="'http://127.0.0.1:30000/images/' + image.uid"
+          />
+        </div>
+        <div class="title">
+          {{ image.path | filename }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,21 +23,41 @@ import { API_SERVICE } from '@/api';
 import { ImageFile, STORE } from '@/store'; // eslint-disable-line no-unused-vars
 
 const ImageGrid = Vue.extend({
+  data() {
+    return {
+      maxSize: 300,
+    }
+  },
+
+  filters: {
+    filename(value: string) {
+      const components = value.split('/');
+      return components[components.length - 1];
+    }
+  },
+
   mounted() {
-    API_SERVICE.fetchRoot();    
+    API_SERVICE.fetchRoot();
   },
 
   computed: {
+    imageBoxStyle(): {[key: string]: string} {
+      return {
+        'width': `${this.maxSize}px`,
+        'height': `${this.maxSize}px`,
+      };
+    },
+
     imageList() {
       const s = STORE.state;
       const newList = [];
-        for (let key in s.images) {
-          newList.push(s.images[key]);
-        }
-        newList.sort((a: ImageFile, b: ImageFile) => {
-          return a.uid.localeCompare(b.uid);
-        });
-        return newList;
+      for (let key in s.images) {
+        newList.push(s.images[key]);
+      }
+      newList.sort((a: ImageFile, b: ImageFile) => {
+        return a.uid.localeCompare(b.uid);
+      });
+      return newList;
     }
   },
 });
@@ -36,5 +66,40 @@ const ImageGrid = Vue.extend({
 export default ImageGrid;
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  .image-box {
+    position: relative;
+
+    .nested {
+      position: absolute;
+      left: 5px;
+      right: 5px;
+      top: 5px;
+      bottom: 30px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      
+      img {
+        display: block;
+        max-width: 100%;
+        max-height: 100%;
+      }
+    }
+
+    .title {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      height: 30px;
+    }
+  }
+}
 </style>
