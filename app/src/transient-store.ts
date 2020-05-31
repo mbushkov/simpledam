427bf40f72d4d0aa765af93d0b8a5532a8d449dc
Mikhail Bushkov <realbushman@gmail.com>
2020-05-31 23:26:06 +0200

    Cleaned up unused stuff from the TransientStore.

--- app/src/transient-store.ts
+++ app/src/transient-store.ts
@@ -6,27 +6,17 @@ export enum ImageViewerTab {
   MEDIA = 1,
 }
 
-export interface SingleImageSettings {
-  autoFit: boolean;
-  scale: number;
-}
-
 export interface TransientState {
   imageViewerTab: ImageViewerTab;
   columnCount: number,
-  singleImageSettings: SingleImageSettings;
 }
 
 type ReadonlyTransientState = Immutable<TransientState>;
 
-class TransientStore {
+export class TransientStore {
   private _state: TransientState = Vue.observable({
     imageViewerTab: ImageViewerTab.THUMBNAILS,
     columnCount: 1,
-    singleImageSettings: {
-      autoFit: true,
-      scale: 100,
-    },
   })
 
   get state(): ReadonlyTransientState {
