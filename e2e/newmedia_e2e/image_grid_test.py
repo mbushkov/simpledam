fec74b801dab6eac1a1c44e3cc44838b023a9e87
Mikhail Bushkov <realbushman@gmail.com>
2020-10-24 11:58:35 +0200

    Cleaned up test utils class and added more tests.

--- e2e/newmedia_e2e/image_grid_test.py
+++ e2e/newmedia_e2e/image_grid_test.py
@@ -1,8 +1,11 @@
 import os
 import shutil
 import tempfile
+from typing import cast
 
 from PIL import Image, ImageDraw
+from newmedia_e2e.lib.base import BrowserWindow
+from selenium.webdriver.common.keys import Keys
 
 from newmedia_e2e.lib import base
 
@@ -11,25 +14,62 @@ class ImageGridTest(base.TestBase):
 
   NUM_IMAGES = 10
 
+  def _GetSortedImages(self, b: BrowserWindow):
+    return sorted(
+        b.GetDisplayedElements('.image-grid .image-box'),
+        key=lambda i: (i.rect["y"], i.rect["x"]),
+    )
+
   def setUp(self):
     self.temp_dir = tempfile.mkdtemp()
     self.addCleanup(shutil.rmtree, self.temp_dir)
 
     for i in range(self.NUM_IMAGES):
       img = Image.new("RGB", (500, 500), color=(20 * i, 20 * i, 20 * i))
-      d = ImageDraw.Draw(img)
+      d = cast(ImageDraw.ImageDraw, ImageDraw.Draw(img))
       d.text((10, 10), "Image %d" % i, fill=(255, 255, 0))
       img.save(os.path.join(self.temp_dir, "%d.png" % i))
 
-  def testDisplaysAllImages(self):
+  def testClickingOnEachImageHighlightsIt(self):
     b = self.CreateWindow(self.temp_dir)
     b.WaitUntilCountEqual(self.NUM_IMAGES, ".image-grid img")
+
+    images = self._GetSortedImages(b)
+    for image in images:
+      b.Click(image)
+      self.assertEqual(image, b.GetDisplayedElement('.image-grid .image-box.selected'))
+
+  def testPressingArrowKeysChangesSelection(self):
+    b = self.CreateWindow(self.temp_dir)
+    b.WaitUntilCountEqual(self.NUM_IMAGES, ".image-grid img")
+
+    images = self._GetSortedImages(b)
+    y_offsets = list(sorted(set(e.rect["y"] for e in images)))
+
+    # Note using jquery:eq might not be the best idea, since by default it also matches invisible elements.
+    b.Click(images[0])
+
+    b.SendKeys(Keys.RIGHT)
+    b.WaitUntilEqual(lambda: b.GetDisplayedElement('.image-grid .image-box.selected'),
+                     lambda: b.GetDisplayedElements('.image-grid .image-box')[1])
+
+    b.SendKeys(Keys.LEFT)
+    b.WaitUntilEqual(lambda: b.GetDisplayedElement('.image-grid .image-box.selected'),
+                     lambda: b.GetDisplayedElements('.image-grid .image-box')[0])
+
+    b.SendKeys(Keys.DOWN)
+    b.WaitUntilEqual(y_offsets[1],
+                     lambda: b.GetDisplayedElement('.image-grid .image-box.selected').rect["y"])
+
+    b.SendKeys(Keys.UP)
+    b.WaitUntilEqual(y_offsets[0],
+                     lambda: b.GetDisplayedElement('.image-grid .image-box.selected').rect["y"])
 
   def testResizinginSingleImageViewHandledCorrectlyByImageGrid(self):
     b = self.CreateWindow(self.temp_dir)
     b.WaitUntilCountEqual(self.NUM_IMAGES, ".image-grid .image-box")
 
-    images = b.GetDisplayedElements('.image-grid .image-box')
+    images = self._GetSortedImages(b)
     # There was a bug when resizing in a single image made the grid switch to a single column.
     # Checking that it doesn't happen.
     x_offsets = set(e.rect["x"] for e in images)
@@ -50,45 +90,3 @@ class ImageGridTest(base.TestBase):
     images = b.GetDisplayedElements('.image-grid .image-box')
     x_offsets = set(e.rect["x"] for e in images)
     self.assertGreater(len(x_offsets), 1)
-
-
-
-      
-     
-       
-  
-            
-           
-        
-      
-     
-       
-  
-            
-           
-        
-           
-             
-                 
-                 
-                                       
-
-      
-     
-       
-  
-            
-           
-        
-      
-     
-       
-  
-            
-           
-        
-           
-             
-                 
-                 
-                                       
--- e2e/newmedia_e2e/lib/base.py
+++ e2e/newmedia_e2e/lib/base.py
@@ -1,10 +1,14 @@
 import os
+import pdb
 import pkg_resources
 import time
 import unittest
 from typing import Any, Callable, List, Optional, TypeVar, Union
 
-from selenium import common, webdriver
+from selenium import webdriver
+from selenium.common import exceptions as selenium_exceptions
+from selenium.webdriver.chrome import service as chrome_service
+from selenium.webdriver.remote import webelement
 
 
 class Error(Exception):
@@ -15,6 +19,14 @@ class ElementNotFoundError(Error):
   pass
 
 
+class MoreThanOneElementFoundError(Error):
+  pass
+
+
+class ExpressionNotEqualError(Error):
+  pass
+
+
 class ConditionNotMetError(Error):
   pass
 
@@ -22,7 +34,7 @@ class ConditionNotMetError(Error):
 # https://www.electronjs.org/docs/api
 class Electron:
 
-  def __init__(self, webdriver: webdriver.Chrome):
+  def __init__(self, webdriver: webdriver.Remote):
     self.wd = webdriver
 
   def OpenDevTools(self):
@@ -31,33 +43,40 @@ class Electron:
   def ResizeWindowBy(self, width: int, height: int):
     self.wd.execute_script(f"window.rawElectron.resizeWindowBy({width}, {height})")
 
+
 T = TypeVar('T')
-WebElement = Union[webdriver.remote.webelement.WebElement]
+
 
 class BrowserWindow:
 
-  def __init__(self, webdriver: webdriver.Chrome):
+  def __init__(self, webdriver: webdriver.Remote):
     self.wd = webdriver
 
   def Close(self):
     self.wd.close()
     self.wd.quit()
 
+  def Breakpoint(self):
+    self.electron.OpenDevTools()
+    pdb.set_trace()
+
   @property
   def electron(self) -> Electron:
     return Electron(self.wd)
 
-  def _FindElements(self, query: str) -> List[WebElement]:
+  def _FindElements(self, query: str) -> List[webelement.WebElement]:
     elems = self.wd.execute_script("return $(\"" + query.replace("\"", "\\\"") + "\");")
     return [e for e in elems if e.is_displayed()]
 
-  def _FindElement(self, query: str) -> WebElement:
+  def _FindElement(self, query: str) -> webelement.WebElement:
     elems = self._FindElements(query)
     if not elems:
       raise ElementNotFoundError(query)
+    if len(elems) > 1:
+      raise MoreThanOneElementFoundError(query)
     return elems[0]
 
-  def WaitUntil(self, fn:Callable[..., T], *args:Any) -> T:
+  def WaitUntil(self, fn: Callable[..., T], *args: Any) -> T:
     max_num = 10
     for i in range(max_num):
       try:
@@ -65,7 +84,7 @@ class BrowserWindow:
         if result:
           return result
         time.sleep(1)
-      except Exception as e:
+      except (Error, selenium_exceptions.WebDriverException) as e:
         if i < max_num - 1:
           time.sleep(1)
         else:
@@ -73,8 +92,25 @@ class BrowserWindow:
 
     raise ConditionNotMetError(f"None of {max_num} attempts returned a truthy value")
 
-  def WaitUntilEqual(self, expected: T, fn: Callable[..., T], *args: Any) -> None:
-    self.WaitUntil(lambda: fn(*args) == expected)
+  def WaitUntilEqual(
+      self,
+      expected: Union[T, Callable[[], T]],
+      fn: Callable[..., T],
+      *args: Any,
+  ) -> None:
+
+    def Check():
+      result = fn(*args)
+      if callable(expected):
+        expected_value = expected()
+      else:
+        expected_value = expected
+      if result != expected_value:
+        raise ExpressionNotEqualError(f"got: {result}, expected: {expected_value}")
+
+      return True
+
+    self.WaitUntil(Check)
 
   def WaitUntilCountEqual(self, expected: int, query: str) -> None:
     self.WaitUntilEqual(expected, lambda: len(self._FindElements(query)))
@@ -82,42 +118,47 @@ class BrowserWindow:
   def WaitUntilPresent(self, query: str) -> None:
     self.WaitUntil(lambda: self._FindElements(query))
 
-  def GetDisplayedElements(self, query: str) -> List[WebElement]:
+  def GetDisplayedElements(self, query: str) -> List[webelement.WebElement]:
     return self.WaitUntil(self._FindElements, query)
 
-  def GetDisplayedElement(self, query: str) -> WebElement:
+  def GetDisplayedElement(self, query: str) -> webelement.WebElement:
     return self.WaitUntil(self._FindElement, query)
 
-  def Click(self, query_or_elem: Union[str, WebElement]) -> None:
-    if isinstance(query_or_elem, webdriver.remote.webelement.WebElement):
+  def Click(self, query_or_elem: Union[str, webelement.WebElement]) -> None:
+    if isinstance(query_or_elem, webelement.WebElement):
       query_or_elem.click()
-    else:      
-      self._FindElement(query_or_elem).click()
+    else:
+      self.GetDisplayedElement(query_or_elem).click()
 
-  def DoubleClick(self, query_or_elem: Union[str, WebElement]) -> None:
-    if isinstance(query_or_elem, webdriver.remote.webelement.WebElement):
+  def DoubleClick(self, query_or_elem: Union[str, webelement.WebElement]) -> None:
+    if isinstance(query_or_elem, webelement.WebElement):
       elem = query_or_elem
     else:
-      elem = self._FindElement(query_or_elem)
-    
+      elem = self.GetDisplayedElement(query_or_elem)
+
     action = webdriver.ActionChains(self.wd)
     action.double_click(elem).perform()
+
+  def SendKeys(self, keys: str):
+    body = self.GetDisplayedElement('body')
+    body.send_keys(keys)
 
 
 class TestBase(unittest.TestCase):
 
-  webdriver_service: webdriver.chrome.service.Service = None
+  webdriver_service: chrome_service.Service
   jquery_source: str = ""
 
   @classmethod
   def setUpClass(cls):
     os.environ["IS_NM_E2E_TEST"] = "1"
 
-    cls.webdriver_service = webdriver.chrome.service.Service("chromedriver")
+    cls.webdriver_service = chrome_service.Service("chromedriver")
     cls.webdriver_service.start()
 
     requirement = pkg_resources.Requirement.parse("newmedia_e2e")
-    with pkg_resources.resource_stream(requirement, "newmedia_e2e/assets/jquery-3.5.1.min.js") as fd:
+    with pkg_resources.resource_stream(requirement,
+                                       "newmedia_e2e/assets/jquery-3.5.1.min.js") as fd:
       cls.jquery_source = fd.read().decode("utf-8")
 
   @classmethod
