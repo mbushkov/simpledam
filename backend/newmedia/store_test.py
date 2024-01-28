e23804ee95f3c2e1aebd341e66af1e5d3b4ddef9
Mikhail Bushkov <realbushman@gmail.com>
2024-01-28 19:44:12 +0100

    Cleaner implementation of DB tests.

--- backend/newmedia/store_test.py
+++ backend/newmedia/store_test.py
@@ -5,8 +5,18 @@ from newmedia import backend_state
 from newmedia import store
 
 import pytest
+import pytest_asyncio
 
 from newmedia import communicator
+
+
+@pytest_asyncio.fixture
+async def db():
+  db = store.DataStore()
+  try:
+    yield db
+  finally:
+    await db.Close()
 
 
 @pytest.mark.asyncio
@@ -15,17 +25,12 @@ from newmedia import communicator
     "BACKEND_STATE",
     backend_state.BackendState(communicator.CommunicatorStub()),
     create=True)
-async def test_SchemaIsUpToDate():
+async def test_SchemaIsUpToDate(db: store.DataStore):
   with open(os.path.join(os.path.dirname(__file__), "store_schema.sql"), "r", encoding="utf-8") as fd:
     expected_schema = fd.read()
 
-  db = store.DataStore()
+  schema = await db.GetSchema()
   try:
-    schema = await db.GetSchema()
-    try:
-      assert schema == expected_schema
-    except AssertionError:
-      print(schema)
-  finally:
-    # TODO: use better resource celanup strategy in tests.
-    await db.Close()
+    assert schema == expected_schema
+  except AssertionError:
+    print(schema)
