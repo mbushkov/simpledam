6d0b3d6283d42fba04aefc203ab5eed77880f6fe
Mikhail Bushkov <realbushman@gmail.com>
2024-02-01 16:13:03 +0100

    Fixed screenshots handling.

--- e2e/newmedia_e2e/conftest.py
+++ e2e/newmedia_e2e/conftest.py
@@ -3,17 +3,19 @@ import os
 
 import pytest
 
+
 @pytest.hookimpl(wrapper=True, tryfirst=True)
 def pytest_runtest_makereport(item, call):
   rep = yield
 
-  if not os.environ["E2E_SCREENSHOT_PATH"]:
+  if not os.environ.get("E2E_SCREENSHOTS_PATH"):
     return rep
 
   if rep.when == "call" and rep.failed and getattr(item.obj.__self__, "last_screenshot_bytes"):
+    date_str = datetime.today().strftime("%Y-%m-%d_%H:%M").replace(":", "_")
     screenshot_path = os.path.join(
-      os.environ["E2E_SCREENSHOT_PATH"], 
-      f'screenshot_{datetime.today().strftime("%Y-%m-%d_%H:%M")}.png',
+        os.environ["E2E_SCREENSHOTS_PATH"],
+        f"screenshot_{date_str}.png",
     )
     with open(screenshot_path, 'wb') as fd:
       fd.write(item.obj.__self__.last_screenshot_bytes)
