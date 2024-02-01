051d9d34eb502d34cc54c5c3bf99ab65f8de48a3
Mikhail Bushkov <realbushman@gmail.com>
2024-02-02 00:45:47 +0100

    Improved screenshots handling.

--- e2e/newmedia_e2e/conftest.py
+++ e2e/newmedia_e2e/conftest.py
@@ -4,6 +4,9 @@ import os
 import pytest
 
 
+_SCREENSHOT_COUNTER = 0
+
+
 @pytest.hookimpl(wrapper=True, tryfirst=True)
 def pytest_runtest_makereport(item, call):
   rep = yield
@@ -12,11 +15,15 @@ def pytest_runtest_makereport(item, call):
     return rep
 
   if rep.when == "call" and rep.failed and getattr(item.obj.__self__, "last_screenshot_bytes"):
+    global _SCREENSHOT_COUNTER
+    
     date_str = datetime.today().strftime("%Y-%m-%d_%H:%M").replace(":", "_")
     screenshot_path = os.path.join(
         os.environ["E2E_SCREENSHOTS_PATH"],
-        f"screenshot_{date_str}.png",
+        f"screenshot_{date_str}_{_SCREENSHOT_COUNTER}.png",
     )
+    _SCREENSHOT_COUNTER += 1
+
     with open(screenshot_path, 'wb') as fd:
       fd.write(item.obj.__self__.last_screenshot_bytes)
     print(f"Saved screenshot at: {screenshot_path}")
